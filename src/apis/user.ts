6c1f6cb6345db2a67d26f4a1fee3d18dba494e3d
hxm <2962695969@qq.com>
2024-12-06 20:28:00 +0800

    2024-12-06 修改api下的user相关接口

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,20 +1,6 @@
 // 用户模块相关所有请求
-import { Result } from '@/types/result';
-import { UserLoginInfo, UserRegisterInfo } from '@/types/user';
+import { UserRegisterInfo } from '@/types/user';
 import { request } from '@/utils';
-import exp from 'constants';
-
-// // User登录参数
-// interface UserLoginInfo {
-//   // 账号
-//   username: string;
-//   // 密码
-//   password: string; 
-//   // 验证码
-//   captcha: string;
-//   // 验证码key
-//   key: string;
-// }
 
 // 用户登录
 export function userLogin(fromData:any) {
@@ -25,9 +11,8 @@ export function userLogin(fromData:any) {
     headers: {
       'Content-Type': 'application/json',
     },
-    data: JSON.stringify(fromData)
+    data: fromData
   })
-  // return request.post('/user-api/user/v1/login', fromData)
 }
 
 // 用户注册
@@ -46,15 +31,3 @@ export function generateCaptcha(){
     method: 'get'
   })
 }
-
-// 获取验证码
-// export function getCaptcha(): Promise<Result<Captcha>> {
-//   return request({
-//     url: '/user-api/user/v1/generateCaptcha',
-//     method: 'get'
-//   }).then(response => ({
-//     code: response.data.code,
-//     message: response.data.message,
-//     data: response.data.data
-//   }));
-// }
